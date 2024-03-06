8c8c23a5459683b431e7e94d0d1af84682394818
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-05 20:15:35 -0500

    User Damola Ogundipe saved changes to main.py at 03/06/2024, 01:15:35 UTC time.

--- b8f6ddc7-4e83-4dda-90ea-8bbc6e09aa2d/main.py
+++ b8f6ddc7-4e83-4dda-90ea-8bbc6e09aa2d/main.py
@@ -1,1 +1,33 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.data import Asset
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        # List of chosen top AI stock tickers.
+        self.tickers = ["GOOGL", "AAPL", "MSFT", "NVDA"]
+        # Keeping it simple, no additional data required beyond price/volume for now.
+        self.data_list = []
+
+    @property
+    def interval(self):
+        # This strategy doesn't depend on frequent updates, daily data is sufficient.
+        return "1day"
+
+    @property
+    def assets(self):
+        # The assets this strategy will operate on - our list of AI stocks.
+        return self.tickers
+
+    @property
+    def data(self):
+        # Return any additional data required by the strategy - empty in this case.
+        return self.data_list
+
+    def run(self, data):
+        # Creating a dictionary to set target allocation for each stock.
+        # We distribute our investment equally among all the chosen stocks.
+        allocation_dict = {ticker: 1.0/len(self.tickers) for ticker in self.tickers}
+
+        # Return the target allocation.
+        # The TargetAllocation object encapsulates our desired allocations.
+        return TargetAllocation(allocation_dict)
